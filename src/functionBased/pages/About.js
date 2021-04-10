0b9e1f69203b5e9e38876dea95455a4df19d62d3
ihposey12 <ihposey@crimson.ua.edu>
2021-04-10 15:50:33 -0700

    added routing, links

--- src/functionBased/pages/About.js
+++ src/functionBased/pages/About.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { Link, useRouteMatch, Route } from 'react-router-dom'
+import SinglePage from './SinglePage'
 
-const About = () => {
+const About = (props) => {
+    const { url, path } = useRouteMatch()
+
     return(
-        <div>
-            Hello from about page
+        <div className='about__content'>
+            <ul className='about__list'>
+                <li>
+                    <Link to={`${url}/about-app`}>About App</Link>
+                </li>
+                <li>
+                    <Link to={`${url}/about-author`}>About Author</Link>
+                </li>
+            </ul>
+            <Route path={`${path}/:slug`}>
+                <SinglePage />
+            </Route>
         </div>
     )
 }
